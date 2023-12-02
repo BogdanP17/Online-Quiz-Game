74db6cb694a54d6818cd31ef28565c9c2f9130f6
Bogdan Petrutescu <petrutescubogdan@yahoo.com>
2023-12-02 09:19:41 +0000

    Add more questsions to the quiz

--- assets/script.js
+++ assets/script.js
@@ -22,12 +22,75 @@ const questions = [
         ]
     },
     {
-        question: "Who win the 2019 world cuo?",
+        question: "Who win the 2020 world cup?",
         answers: [
             { text: "France", correct: false},
             { text: "Romania", correct: false},
             { text: "Argentina", correct: true},
             { text: "Germany", correct: false},
+        ]
+    },
+    {
+        question: "How many ballon d'or Messi have?",
+        answers:[
+            { text: "1", correct: false},
+            { text: "5", correct: false},
+            { text: "3", correct: false},
+            { text: "8", correct: true},
+        ]
+    },
+    {
+        question: "Which team Ronaldo play?",
+        answers: [
+            { text:"Read Madrid", correct:false},
+            { text:"Al-Nassr FC", correct: true},
+            { text:"Mancester City", correct: false},
+            { text:"Licerpool", correct: false},
+        ]
+    },
+    {
+        question: "How many goals has Mbappe scored?",
+        answers: [
+            { text:"155", correct: false},
+            { text:"302", correct: true},
+            { text:"200", correct:false},
+            { text:"100", correct: false},
+        ]
+    },
+    {
+        question: "Who is the captain of PSG?",
+        answers: [
+            { text:"Marquinhos", correct: true},
+            { text:"Kylian Mbappe", correct:false},
+            { text:"Danilo Pereira", correct:false},
+            { text:"Julian Draxler", correct:false},
+        ]
+    },
+    {
+        question: "Where is originally Neymar from?",
+        answers: [
+            { text:"Morocco", correct:false},
+            { text:"England", correct:false},
+            { text:"Brazil", correct:true},
+            { text:"France", correct:false},
+        ]
+    },
+    {
+        question: "Which stadium is the biggest in the world?",
+        answers: [
+            { text:"Wembley Stadium", correct:false},
+            { text:"Neyland Stadium", correct: false},
+            { text:"Beaver Stadium", correct:false},
+            { text:"Rungrado 1st of May Stadium", correct:true},
+        ]
+    },
+    {
+        question: "How many yellow card a player can take?",
+        answers: [
+            { text:"5", correct: false},
+            { text:"3", correct:false},
+            { text:"1", correct:false},
+            { text:"2", correct:true},
         ]
     }
 
@@ -100,8 +163,6 @@ const questions = [
     nextButton.style.display = "block";
  }
 
-
-
  function handleNextButton(){
     currentQuestionIndex++;
     if(currentQuestionIndex < questions.length){
@@ -118,5 +179,7 @@ const questions = [
         startGame();
     }
  })
+
+ 
 
 startGame();
